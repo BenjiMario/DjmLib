aafe750f3306e38c6cae7724bb30c038bd7feb29
BENJI <benjamin@ctexdev.net>
2019-03-19 18:55:01 +0100

    libalone

--- app/src/main/java/com/example/djmsdk/MainActivity.java
+++ app/src/main/java/com/example/djmsdk/MainActivity.java
@@ -2,93 +2,16 @@ package com.example.djmsdk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.example.djmlib.model.Card;
-import com.example.djmlib.SdkPublicInterface;
-import com.example.djmlib.StaticSdk;
+public class MainActivity extends AppCompatActivity{
 
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity
-        implements SdkPublicInterface {
-
-    public final static String DEBUGAPP = "DJMAPP";
-
-    private StaticSdk sdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.tvtest);
-       // sdk = StaticSdk.getInstance();
-
-        //sdk.registerObserver(this);
-
-  /*      sdk.enrollToService(this, "Ben", "Leeroy",
-                "toto@gmail.com", Utils.getFCMToken(this));
-*/
-
-   //    sdk.addCard(this);
-
-
-//        sdk.deleteCard(this, new Card());
-//        sdk.getAllCards(this);
-
-
-
-
     }
-
-
-
-    @Override
-    public void onServiceAvailable(String token) {
-        Log.e(DEBUGAPP, "Service Ready : Djm TOKEN : " + token);
-    }
-
-    @Override
-    public void onCardAvailable(boolean ok, Card card) {
-        if (ok) {
-            Log.e(DEBUGAPP, "Card Added :\n" + card);
-        } else {
-            Log.e(DEBUGAPP, "Error getting card :\n" + card);
-        }
-    }
-
-    @Override
-    public void onCardDeleted(boolean ok) {
-        if (ok) {
-            Log.e(DEBUGAPP, "Card Deleted");
-        } else {
-            Log.e(DEBUGAPP, "Error deleting Card");
-        }
-    }
-
-    @Override
-    public void onCardListReceived(ArrayList<Card> cards) {
-        if (cards.size() == 0) {
-            Log.e(DEBUGAPP, "NO Cards Found");
-        } else {
-            for (Card c : cards) {
-                Log.e(DEBUGAPP, c.toString());
-            }
-        }
-    }
-
-    @Override
-    public void onAnalyticsDataReceived(boolean ok, JSONArray data) {
-
-    }
-
-    @Override
-    public void onPaymentDone(boolean ok) {
-
-    }
-
 
 }
